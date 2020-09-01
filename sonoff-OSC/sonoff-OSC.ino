9c84723c6b638649d26bf528619c8594293bab84
j t f <rrmutt@gmail.com>
2020-08-31 20:45:34 -0700

    checkpoint

--- sonoff-OSC/sonoff-OSC.ino
+++ sonoff-OSC/sonoff-OSC.ino
@@ -6,6 +6,11 @@
 // install via library manager
 
 
+#include <Debouncer.h>
+//https://github.com/hideakitai/Debouncer
+// install via library manager
+
+
 // board install instructions: https://github.com/esp8266/Arduino
 
 // Select "Generic ESP8285" in board manager
@@ -28,27 +33,25 @@
 */
 
 
-// watchdog problems use https://github.com/me-no-dev/EspExceptionDecoder#installation
+
 
 #include "credentials.h"
-
 
 // Constants from credentials.h so we don't check them into git!
 //const char* ssid = "SSID";
 //const char* pwd = "Password";
 
-
+#define PUSHBUTTON 0
 #define RELAY_PIN 12
 #define LED_PIN 13
 
-// milliseconds to time out and turn off relay
-// zero value indicates timer is not running
-unsigned long relay_timeout = 0;
 
 //todo: switch colors etc. from touchOSC
 
 
-const IPAddress ip(192, 168, 1, 220);
+#define ADDR 5
+
+const IPAddress ip(192, 168, 1, 220 + ADDR);
 const IPAddress gateway(192, 168, 1, 1);
 const IPAddress subnet(255, 255, 255, 0);
 
@@ -60,8 +63,19 @@ const char* host = "192.168.1.149";
 const int recv_port = 10000;
 const int send_port = 12000;
 
+const int bind_port = 54345;
+const int publish_port = 54445;
+
+int relay_stat = LOW;
 
 
+// values for timers
+#define DEBOUNCE_DELAY 10
+unsigned long timer_start = 0;
+// milliseconds to time out and toggle relay
+// zero value indicates timer is not running
+unsigned long timer_delay_ms = 0;
+Debouncer button(PUSHBUTTON, DEBOUNCE_DELAY);
 
 void setup() {
   Serial.begin(115200);
@@ -77,6 +91,9 @@ void setup() {
   }
   Serial.print("\n\nWiFi connected, IP = "); Serial.println(WiFi.localIP());
 
+  //OscWiFi.publish(host, publish_port, "/publish/value", relay_stat)
+  //->setFrameRate(10.f);
+
   OscWiFi.subscribe(recv_port, "/switch",
                     [](const OscMessage & m)
   {
@@ -84,24 +101,51 @@ void setup() {
     Serial.print(m.remotePort()); Serial.print(" ");
     Serial.print(m.size()); Serial.print(" ");
     Serial.print(m.address()); Serial.print(" ");
-    Serial.print(m.arg<int>(0)); Serial.print(" ");
-    //Serial.print(m.arg<float>(1)); Serial.print(" ");
-    //Serial.print(m.arg<String>(2)); Serial.println();
+    Serial.println(m.arg<int>(0)); Serial.print(" ");
+    if (m.size() > 1) {
+      Serial.print(m.arg<float>(1)); Serial.println(" ");
+      timer_start = millis();
+      timer_delay_ms = (unsigned long)(1000L * m.arg<float>(1));
+      Serial.print("Timer set for ms ");
+      Serial.println(timer_delay_ms);
+    }
+
 
     if ((m.arg<int>(0)) > 0) {
-      digitalWrite(LED_PIN, HIGH);
-      digitalWrite(RELAY_PIN, HIGH);
-
+      set_relay(1);
     }
     else {
-      digitalWrite(LED_PIN, LOW);
-      digitalWrite(RELAY_PIN, LOW);
-
+      set_relay(0);
     }
   }
                    );
 
 
+  OscWiFi.subscribe(recv_port, "/toggle",
+                    [](const OscMessage & m)
+  {
+    Serial.print(m.remoteIP()); Serial.print(" ");
+    Serial.print(m.remotePort()); Serial.print(" ");
+    Serial.println(m.address()); Serial.print(" ");
+    toggle();
+
+  }
+
+                   );
+
+  OscWiFi.subscribe(recv_port, "/status",
+                    [](const OscMessage & m)
+  {
+    Serial.print(m.remoteIP()); Serial.print(" ");
+    Serial.print(m.remotePort()); Serial.print(" ");
+    Serial.println(m.address()); Serial.print(" ");
+    post_status();
+  }
+
+                   );
+
+
+  pinMode(PUSHBUTTON, INPUT_PULLUP);
 
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
@@ -110,21 +154,61 @@ void setup() {
 
   Serial.println("START");
 
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT); // inverted, low to light
+  digitalWrite(LED_PIN, HIGH);
+
+
+  // blink address on LED on startup
+  for (int i = 0; i < ADDR; i++) {
+    digitalWrite(LED_PIN, LOW);
+    delay(300);
+    digitalWrite(LED_PIN, HIGH);
+    delay(100);
+  }
+  digitalWrite(LED_PIN, LOW);
 
 }
 
 void loop() {
 
-  OscWiFi.update();
-  //  digitalWrite(LED_PIN, HIGH);
-  //  digitalWrite(RELAY_PIN, HIGH);
-  //  delay(500);
-  //
-  //  digitalWrite(LED_PIN, LOW);
-  //  digitalWrite(RELAY_PIN, LOW);
-  //  delay(500);
-  //
-  //  Serial.println("LED");
+  //OscWiFi.update();
+  OscWiFi.update(); // to receive osc
 
+  button.update();
+  if (button.edge()) {
+    if (button.falling())        {
+      Serial.print("fall : ");
+      toggle();
+
+    }
+  }
+
+  if (timer_delay_ms) {
+    if ((millis() - timer_start) > timer_delay_ms) {
+      Serial.println("Timer up!");
+      toggle();
+      timer_delay_ms = 0;
+    }
+  }
+}
+
+void toggle(void) {
+  set_relay(1 - relay_stat);
+}
+
+void set_relay(byte value) {
+  relay_stat = value;
+  digitalWrite(LED_PIN, relay_stat);
+  digitalWrite(RELAY_PIN, relay_stat);
+  Serial.print("Set relay:");
+  Serial.println(relay_stat);
+  post_status();
+
+}
+
+
+void post_status(void) {
+  OscWiFi.send(host, send_port, "/status", relay_stat);
+
+  //OscWiFi.post();
 }
--- sonoff-logic.py
+++ sonoff-logic.py
@@ -5,8 +5,8 @@ import time
 
 osc_startup()
 
-osc_udp_client("192.168.1.220", 10000, "client_send")
-osc_udp_client("192.168.1.220", 54345, "client_bind")
+osc_udp_client("192.168.1.225", 10000, "client_send")
+osc_udp_client("192.168.1.225", 54345, "client_bind")
 osc_udp_server("0.0.0.0", 12000, "server_recv")
 osc_udp_server("0.0.0.0", 54445, "server_published")
 
@@ -17,16 +17,18 @@ osc_method("/*", handler, argscheme=osm.OSCARG_ADDRESS + osm.OSCARG_DATAUNPACK)
 
 try:
     while True:
-        msg = oscbuildparse.OSCMessage('/switch', ",i", [1])
+        #msg = oscbuildparse.OSCMessage('/switch', ",if", [1, 2.0])
+        msg = oscbuildparse.OSCMessage('/status', ",i", [0])
         osc_send(msg, "client_send")
         osc_process() # one message, one call
-
+        print("sent on")
         time.sleep(1)
         
-        msg = oscbuildparse.OSCMessage('/switch', ",i", [0])
-        osc_send(msg, "client_send")
-        osc_process() # one message, one call
-        time.sleep(1)
+        # msg = oscbuildparse.OSCMessage('/switch', ",if", [0, 0.0])
+        # osc_send(msg, "client_send")
+        # osc_process() # one message, one call
+        # print("sent off")
+        # time.sleep(0.25)
 
 
 except KeyboardInterrupt:
